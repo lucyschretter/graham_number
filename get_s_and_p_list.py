7a0377d9bc19e4e038e9b6d45b68fd5adc00dc85
Lucy Schretter <lucy_emilia.schretter@smail.th-koeln.de>
2023-11-14 09:51:29 +0100

    Get quarterly list of companies in the S&P 500 index from csv file where dates range from 1996-01-02 to 2023-06-20; get ticker list for target date, get most common and unique tickers over the time

---
+++ get_s_and_p_list.py
@@ -0,0 +1,71 @@
+"""
+get via csv file from github
+source code: https://github.com/fja05680/sp500/blob/master/sp500_by_date.ipynb
+
+dates range from 1996-01-02 to 2023-06-20
+source csv: https://github.com/fja05680/sp500/blob/master/S%26P%20500%20Historical%20Components%20%26%20Changes(08-01-2023).csv
+"""
+
+import pandas as pd
+
+
+def get_quartery_df(filename: str):
+    df = pd.read_csv(filename, index_col='date')
+    df.index = pd.to_datetime(df.index)
+    df['tickers'] = df['tickers'].apply(lambda x: sorted(x.split(',')))
+    quarterly_df = df.resample('Q').first()
+
+    return quarterly_df
+
+
+def get_ticker_list_for_target_date(target_date: str):
+    """
+    :param target_date: target_date has to be the last day of a quarter
+    :return: list
+    """
+
+    df = get_quartery_df('S&P 500 Historical Components & Changes(08-01-2023).csv')
+
+    # Loop through the quarterly DataFrame and get historical data for each quarter
+    for index, row in df.iterrows():
+        tickers = row['tickers']
+        date_str = index.strftime("%Y-%m-%d")
+
+        if date_str == target_date:
+            return tickers
+
+
+def get_most_common_tickers():
+    df = get_quartery_df('S&P 500 Historical Components & Changes(08-01-2023).csv')
+
+    # Initialize the list with companies from the first row
+    common_tickers = list(df.iloc[0]['tickers'])
+
+    # Loop through the quarterly DataFrame and get historical data for each quarter
+    for index, row in df.iterrows():
+        tickers = row['tickers']
+
+        # Update the list with the intersection of current tickers and previous tickers
+        common_tickers = list(set(common_tickers) & set(tickers))
+
+    return sorted(common_tickers)
+
+
+def get_unique_companies():
+    df = get_quartery_df('S&P 500 Historical Components & Changes(08-01-2023).csv')
+
+    # Create an empty set to store unique tickers
+    unique_tickers = set()
+
+    # Loop through the DataFrame and add all unique tickers to the set
+    for tickers_list in df['tickers']:
+        unique_tickers.update(tickers_list)
+
+    # Convert the set to a list for printing or further use
+    unique_tickers_list = list(unique_tickers)
+
+    # Print the list of unique tickers
+    return sorted(unique_tickers_list)
+
+
+print(len(get_unique_companies()))
